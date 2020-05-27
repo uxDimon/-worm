8e7174431992c44e06f9e6933a1d17b03b5cfc0e
Shelkov Dmitriy <bobyorrr@gmail.com>
2020-05-27 20:10:28 +0300

    червяк

--- main.js
+++ main.js
@@ -1,89 +1,93 @@
-var canvas1 = document.getElementById("canvas1");
-var context = canvas1.getContext("2d");
+const canvas1 = document.getElementById("canvas1");
+const context = canvas1.getContext("2d");
 
-// canvas1.onmousedown = function () {
-// 	canvas1.onmousemove = function (event) {
-// 		var x = event.offsetX;
-// 		var y = event.offsetY;
+const beckGlif = ".";
+let text2d = [];
+let colum = 76;
+let row = 57;
 
-// 		context.fillStyle = "#fff";
-// 		context.fillRect(x, y, 5, 5);
-// 	};
-// 	canvas1.onmouseup = function () {
-// 		canvas1.onmousemove = null;
-// 	};
-// };
-
-// var upx;
-// var upy;
-
-// var x;
-// var y;
-
-// canvas1.onmousedown = function (event) {
-// 	x = event.offsetX;
-// 	y = event.offsetY;
-
-// 	canvas1.onmousemove = function (event) {
-// 		upx = event.offsetX;
-// 		upy = event.offsetY;
-
-// 		context.clearRect(0, 0, 500, 500);
-// 		context.beginPath();
-// 		context.strokeStyle = "#fff";
-// 		context.lineWidth = 1;
-// 		context.moveTo(x, y);
-// 		context.lineTo(upx, upy);
-// 		context.stroke();
-// 	};
-
-// 	canvas1.onmouseup = function (event) {
-// 		canvas1.onmousemove = null;
-// 	};
-// };
-
-var arr = [[]];
-var number = 0;
-var newObg = true;
-
-function lastPointX() {
-	return arr[number][arr[number].length - 1].x;
-}
-function lastPointY() {
-	return arr[number][arr[number].length - 1].y;
-}
-
-function startArrow(x, y, arrow = 20) {
-	return x >= arr[number][0].x - arrow && x <= arr[number][0].x + arrow && y >= arr[number][0].y - arrow && y <= arr[number][0].y + arrow;
-}
-
-canvas1.onclick = function (event) {
-	context.beginPath();
-	context.strokeStyle = "#fff";
-	context.lineWidth = 5;
-	if (newObg) {
-		arr[number].push({ x: event.offsetX, y: event.offsetY });
-
-		if (arr[number].length == 1) {
-			context.moveTo(lastPointX(), lastPointY());
-
-			console.log("1");
-		} else if (startArrow(event.offsetX, event.offsetY)) {
-			context.fillStyle = "rgba(255, 255, 255, 0.25)";
-			context.fill();
-			context.closePath();
-
-			newObg = false;
-			console.log("2");
-		} else {
-			context.lineTo(lastPointX(), lastPointY());
-			context.stroke();
-
-			console.log("3");
+function created2dText() {
+	for (let c = 0; c < colum; c++) {
+		text2d.push([]);
+		for (let r = 0; r < row; r++) {
+			text2d[c].push(beckGlif);
 		}
-		context.stroke();
-	} else {
-		++number;
-		arr.push([]);
 	}
+}
+
+function textGene() {
+	let newStrPos = 0; // отступ сверхуъ
+	let strText = "";
+
+	for (let c of text2d) {
+		for (let r of c) {
+			strText += r;
+		}
+		context.fillText(strText, 0, newStrPos);
+		strText = "";
+		newStrPos = newStrPos + 12;
+	}
+}
+
+function changeNumArray(r, c) {
+	if (c < colum && r < row) {
+		let num = 10000000000;
+		text2d[c][r] = num;
+
+		function tm() {
+			setTimeout(() => {
+				if (num > 9) {
+					num = Math.round((num / 100) * 10);
+					text2d[c][r] = num;
+					tm();
+				} else {
+					text2d[c][r] = beckGlif;
+				}
+			}, 100);
+		}
+		tm();
+	}
+}
+
+// function sinArrey() {
+// 	let x = 0;
+// 	let y = 0;
+
+// 	function stepsSin() {
+// 		setTimeout(() => {
+// 			y = Math.sin(x);
+// 			// y = 25 + 10 * y;
+// 			y = 25 + 10 * ((y - (y / 100) * 50) / 3);
+// 			x += 1;
+// 			changeNumArray(Math.round(y), Math.round(x));
+// 			// changeNumArray(Math.round(y) + 1, x);
+// 			// changeNumArray(Math.round(y) + 2, x);
+// 			console.log(Math.round(y));
+// 			if (x >= colum) {
+// 				x = 0;
+// 			}
+// 			stepsSin();
+// 		}, 100);
+// 	}
+// 	stepsSin();
+// }
+
+function loop() {
+	context.clearRect(0, 0, 1000, 1000);
+
+	textGene();
+	requestAnimationFrame(loop);
+}
+
+context.fillStyle = "#9E9E9E";
+context.font = "16px 'Anonymous Pro'";
+loop();
+created2dText();
+
+// sinArrey();
+
+canvas1.onmousemove = (event) => {
+	let r = Math.round(event.offsetX / 8.8);
+	let c = Math.round(event.offsetY / 12);
+	changeNumArray(r, c);
 };
